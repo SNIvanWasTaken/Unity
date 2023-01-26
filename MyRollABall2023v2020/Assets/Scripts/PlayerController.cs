fe0163fc6bf4221d30ac79c430f5b6fabd062d98
SN_Ivan <ivalagmor@alu.edu.gva.es>
2023-01-26 18:27:30 +0100

    Practica 3D

--- MyRollABall2023v2020/Assets/Scripts/PlayerController.cs
+++ MyRollABall2023v2020/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,25 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+
 	// Create public variables for player speed, and for the Text UI game objects
-	public float speed;
+	float velocidadAvance = 2.0f;
+	float velocidadAvanceZ = 6.0f;
+	public float salto = 5;
 	public Text countText;
 	public Text winText;
 
 	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
 	private Rigidbody rb;
 	private int count;
+	private Renderer re;
 
 	// At the start of the game..
 	void Start ()
 	{
 		// Assign the Rigidbody component to our private rb variable
 		rb = GetComponent<Rigidbody>();
-
+		re = GetComponentInChildren<Renderer>();
 		// Set the count to zero 
 		count = 0;
 
@@ -32,19 +35,27 @@ public class PlayerController : MonoBehaviour {
 		winText.text = "";
 	}
 
-	// Each physics step..
-	void FixedUpdate ()
+    private void Update()
+    {
+		if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.1f)
+        {
+			rb.AddForce(Vector3.up * salto, ForceMode.Impulse);
+        }
+    }
+    // Each physics step..
+    void FixedUpdate ()
 	{
 		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Vector3 movement = new Vector3 (moveVertical, 0.0f, -moveHorizontal);
 
 		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
 		// multiplying it by 'speed' - our public player speed that appears in the inspector
-		rb.AddForce (movement * speed);
+		rb.AddForce (movement * velocidadAvance);
+		rb.AddForce(movement * velocidadAvanceZ);
 	}
 
 	// When this game object intersects a collider with 'is trigger' checked, 
@@ -72,10 +83,17 @@ public class PlayerController : MonoBehaviour {
 		countText.text = "Count: " + count.ToString ();
 
 		// Check if our 'count' is equal to or exceeded 12
-		if (count >= 12) 
+		if (count >= 15) 
 		{
 			// Set the text value of our 'winText'
 			winText.text = "You Win!";
 		}
 	}
+    private void OnCollisionEnter(Collision collision)
+    {
+		if (collision.gameObject.CompareTag("Wall"))
+		{
+			re.material.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
 }
